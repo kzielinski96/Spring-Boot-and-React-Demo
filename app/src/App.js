9a4ae90e108a6e6d0b666e8b2e45d9fe78ff3307
kzielinski96 <kornel.zielinski96@gmail.com>
2019-04-14 17:34:46 +0200

    Added students list and some basic functionality to the API

--- app/src/App.js
+++ app/src/App.js
@@ -1,39 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import StudentsList from './StudentsList';
 
 class App extends Component {
-  state = {
-    isLoading: true,
-    students: []
-  };
-
-  async componentDidMount() {
-    const response = await fetch('api/students');
-    const body = await response.json();
-    this.setState({ students: body, isLoading: false });
-  }
   render() {
-    const { students, isLoading } = this.state;
-
-    if (isLoading) {
-      return <p>Loading...</p>;
-    }
-
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <div className="App-intro">
-            <h2>Students List</h2>
-            {students.map(student => (
-              <div key={student.id}>{student.name}</div>
-            ))}
-          </div>
-        </header>
-      </div>
-    );
+        <Router>
+          <Switch>
+            <Route path='/' exact={true} component={Home}/>
+            <Route path='/students' exact={true} component={StudentsList}/>
+          </Switch>
+        </Router>
+    )
   }
 }
-
 export default App;
---
+++ app/src/StudentsList.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import { Button, ButtonGroup, Container, Table } from 'reactstrap';
+import AppNavbar from './AppNavbar';
+import { Link } from 'react-router-dom';
+
+class StudentsList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {students: [], isLoading: true};
+        this.remove = this.remove.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({isLoading: true});
+
+        fetch('api/students')
+            .then(response => response.json())
+            .then(data => this.setState({students: data, isLoading: false}));
+    }
+
+    async remove(id) {
+        await fetch(`/api/student/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            let updatedStudents = [...this.state.students].filter(i => i.id !== id);
+            this.setState({students: updatedStudents});
+        });
+    }
+
+    render() {
+        const {students, isLoading} = this.state;
+
+        if (isLoading) {
+            return <p>Loading...</p>
+        }
+
+        const studentsList = students.map(student => {
+            return  <tr key={student.id}>
+                        <td style={{whiteSpace: 'nowrap'}}>{student.name}</td>
+                        <td>{student.album}</td>
+                        <td>{student.mail}</td>
+                        <ButtonGroup>
+                            <Button size="sm" color="primary" tag={Link} to={"/students/" + student.id}>Edit</Button>
+                            <Button size="sm" color="danger" onClick={() => this.remove(student.d)}>Delete</Button>
+                        </ButtonGroup>
+                    </tr>
+        });
+
+        return (
+            <div>
+                <AppNavbar/>
+                <Container fluid>
+                    <div className="float-right">
+                        <Button color="success" tag={Link} to="/students/new">Add Student</Button>
+                    </div>
+                    <h3>Students List</h3>
+                    <Table className="mt-4">
+                        <thead>
+                        <tr>
+                            <th width="20%">Name</th>
+                            <th width="20%">Album</th>
+                            <th width="20%">Mail</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {studentsList}
+                        </tbody>
+                    </Table>
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default StudentsList;
